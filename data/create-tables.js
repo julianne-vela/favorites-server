7fa089c3e6972e0e6ed2eefcf2ed04ce1ebef008
Julianne Vela <cjvela@outlook.com>
2021-03-03 21:29:36 -0800

    updated data type for user.name to hopefully clear null issue

--- data/create-tables.js
+++ data/create-tables.js
@@ -13,7 +13,7 @@ async function run() {
 		await client.query(`
                 CREATE TABLE users (
                     id SERIAL PRIMARY KEY,
-                    name VARCHAR(256) NOT NULL,
+                    name VARCHAR(512) NOT NULL,
                     email VARCHAR(256) NOT NULL,
                     hash VARCHAR(512) NOT NULL
                 );           
